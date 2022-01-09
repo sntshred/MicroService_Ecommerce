b9cc37ea9c9f39ee669ce59cd951870ef7608b33
Santosh Chavan <santosh.aryan9@gmail.com>
2022-01-09 17:59:23 -0500

    added ordering with docker container

---
+++ src/Services/Ordering/Ordering.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Persistence;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Infrastructure.Repositories;
+using Ordering.Application.Models;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Infrastructure.Mail;
+
+namespace Ordering.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString"));
+
+            });
+            // the typeof required by mediatR for creating repository related objects
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
